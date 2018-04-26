4fa1c42f43bfbf3e39cdea5505d144d6cf46c237
Maken Hubbard <makhub26@outlook.com>
2018-04-25 19:19:25 -0500

    adding the getter and setter for the data.js and cleaned up some console.log and tested it

---
+++ javascripts/data.js
@@ -0,0 +1,14 @@
+let messagesArray = [];
+
+const setMessages = (messArray) => {
+  messagesArray = messArray;
+};
+
+const getMessages = () => {
+  return messagesArray;
+};
+
+module.exports = {
+  setMessages,
+  getMessages,
+};
