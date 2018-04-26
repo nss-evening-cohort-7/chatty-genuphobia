5139d676b72c48d42bafff4db10249ec11469adb
Alicia Zaludova <aliciazaludova@gmail.com>
2018-04-25 20:38:17 -0500

    messages printing

---
+++ javascripts/chattyDom.js
@@ -0,0 +1,23 @@
+// const data = require('./data');
+
+// const events = require('./events');
+
+const messagesOutputDiv = document.getElementById('message-container');
+
+const domStrang = (messages) => {
+  let domStrang = '';
+  messages.forEach((message) =>
+  {
+    domStrang += `<div id="msgs">`;
+    domStrang += `<p id="${message.id}">${message.message}</p>`;
+    domStrang += `<button id="delete-btn">Delete</button>`;
+    domStrang += `</div>`;
+  });
+  return domStrang;
+};
+
+const printMessagesToDom = (messages) => {
+  messagesOutputDiv.innerHTML = domStrang(messages);
+};
+
+module.exports = printMessagesToDom;
--- javascripts/dataGateKeeper.js
+++ javascripts/dataGateKeeper.js
@@ -1,12 +1,13 @@
 const data = require('./data');
 const loadMessage = require('./chatty');
-// const chattyDom = require('chattyDom');
+const chattyDom = require('./chattyDom');
 
 const onLoadFunction = function () {
   // the line below is parsing the data
   const messagesData = JSON.parse(this.responseText).messages;
   // this is choosing which functions that were exported in data.js by using the dot
   data.setMessages(messagesData);
+  chattyDom(messagesData);
 };
 
 const failToLoad = function () {
